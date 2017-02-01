d270d0d33b45da30a7b3589e82d3c8d81ec6072e
Wunkolo <Wunkolo@gmail.com>
2017-02-01 02:02:58 -0800

    Update lexer test program

--- OakSim/src/Main.java
+++ OakSim/src/Main.java
@@ -7,15 +7,24 @@ import ARMLex.*;
 public class Main {
 	public static void main(String[] Args)
 	{
-		String TestProgram = "main: @Start of the program\n\tldr r0, [r1]\n\tmov    r0, #(0x1 * 5)\n\tmov    r0, #01\n\tbx      lr";
+		String TestProgram = 
+				".global _start\n"
+				+ "_start: @Start of the program\n"
+				+ "\tldr r0, [r1]\n"
+				+ "\tstmia   r0!, {r3}\n"
+				+ "\tldr     r3, [r1, r2, lsl #2]\n"
+				+ "\tldr r0, [r1]\n"
+				+ "\tadd     r2, r0, #16\n"
+				+ "\tldr     r1,=obj_buffer\n"
+				+ "\tldrcs   r3, [r1], #4\n"
+				+ ".ascii \"Hello World\\n\"\n"
+				+ "\tbx main\n";
 		
 		ArrayList<ARMLex.Token> Tokens = ARMLex.Lexigraph(TestProgram);
 		
 		for( ARMLex.Token CurToken : Tokens)
 		{
 			System.out.println(CurToken);
-		}
-		
-		
+		}		
 	}
 }
