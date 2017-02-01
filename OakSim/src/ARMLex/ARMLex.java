fbb5f1cd2e65ff00a7d178e533740a7d456f67d2
Wunkolo <Wunkolo@gmail.com>
2017-02-01 00:18:58 -0800

    Add simple arm lexer

---
+++ OakSim/src/ARMLex/ARMLex.java
@@ -0,0 +1,112 @@
+package ARMLex;
+
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class ARMLex
+{
+	public static enum TokenType
+	{
+		// regex expressions of all Lexemes we care for
+		// Note that order matters as all these regex expressions will be
+		// grouped up in the order that they are described here		
+		WHITESPACE("[ \t\f\r]+"),
+		NEWLINE("\n"),
+		COMMENT("@.+$"),
+		
+		LABEL("^.+:"),
+		
+		ARITHMETIC("[*|/|+|-]"),
+		
+		BRACKETOPEN("\\["),
+		BRACKETCLOSE("\\]"),
+		
+		PARENTHOPEN("\\("),
+		PARENTHCLOSE("\\)"),
+		
+		IMMEDIATE("#"),
+		HEXIDECIMAL("0[xX][0-9a-fA-F]+"),
+		DECIMAL("[0-9]+"),
+		
+		REGISTER("(([rR](\\d|(1[0-2])))|lr|sp|pc|apsr)"),
+		
+		COMMA(","),
+		STRING("\".+\""),
+		WORD("\\w+");
+
+		public final String Pattern;
+
+		private TokenType(String Pattern)
+		{
+			this.Pattern = Pattern;
+		}
+	}
+
+	public static class Token
+	{
+		public TokenType Type;
+		public String Value;
+
+		public Token( TokenType Type, String Value )
+		{
+			this.Type = Type;
+			this.Value = Value;
+		}
+		
+		@Override
+		public String toString()
+		{
+			return String.format(
+					"( %s , \"%s\")",
+					this.Type.name(),
+					this.Value
+					);
+		}
+	}
+	
+	public static ArrayList<Token> Lexigraph(String Input)
+	{
+		ArrayList<Token> Tokens = new ArrayList<Token>();
+		
+		StringBuffer Patterns = new StringBuffer();
+		
+		for(TokenType CurTokenType : TokenType.values())
+		{
+			// Generate string of named regex groups using all token enums
+			// Named regex groups take the format "(?<group name> pattern )"
+			Patterns.append(
+				String.format(
+					"|(?<%s>%s)",
+					CurTokenType.name(),
+					CurTokenType.Pattern
+					)
+				);
+		}
+		
+		// Clips the first '|' character
+		Pattern TokenPatterns = Pattern.compile(
+			new String(Patterns.substring(1)),
+			Pattern.MULTILINE
+			);
+		
+		Matcher TokenMatcher= TokenPatterns.matcher(Input);
+		
+		while(TokenMatcher.find())
+		{
+			for( TokenType CurTokenType : TokenType.values())
+			{
+				if(TokenMatcher.group(CurTokenType.name()) != null)
+				{
+					Tokens.add(
+						new Token(CurTokenType,TokenMatcher.group(CurTokenType.name()))
+						);
+				}
+			}
+		}
+		
+		return Tokens;
+	}
+	
+	
+}
--- OakSim/src/Main.java
+++ OakSim/src/Main.java
@@ -1,7 +1,21 @@
 
+
+
+import java.util.ArrayList;
+import ARMLex.*;
+
 public class Main {
 	public static void main(String[] Args)
 	{
-		System.out.println("Hello Group 1!!");
+		String TestProgram = "main: @Start of the program\n\tldr r0, [r1]\n\tmov    r0, #(0x1 * 5)\n\tmov    r0, #01\n\tbx      lr";
+		
+		ArrayList<ARMLex.Token> Tokens = ARMLex.Lexigraph(TestProgram);
+		
+		for( ARMLex.Token CurToken : Tokens)
+		{
+			System.out.println(CurToken);
+		}
+		
+		
 	}
 }
