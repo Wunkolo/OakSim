184b8e15097de3c7d283f0bdc564e5a195460de1
Wunkolo <wunkolo@gmail.com>
2017-03-27 12:50:59 -0700

    Update sample code formatting

--- SampleCode.s
+++ SampleCode.s
@@ -2,40 +2,39 @@
 #.thumb @ Emit 16-bit THUMB code, note now "IntegerDivide" collapses
 
 IntegerDivide:
-  mov r1, #0
-  adds r0, r0, r0
-  .rept 32			@ repeat 32 times
+	mov r1, #0
+	adds r0, r0, r0
+	.rept 32			@ repeat 32 times
 	adcs r1, r2, r1, lsl #1
 	subcc r1, r1, r2
 	adcs r0, r0, r0
-  .endr
-  mov pc, lr
+	.endr
+	mov pc, lr
 
 Mul50Int:
-  mov     r3, #50
-  mul     r0, r3, r0
-  bx      lr
+	mov     r3, #50
+	mul     r0, r3, r0
+	bx      lr
 		
 
 Mul50Float:
-  vmov    s14, r0
-  vldr.32 s15, .L3
-  vmul.f32        s15, s14, s15
-  vmov    r0, s15
-  bx      lr
+	vmov    s14, r0
+	vldr.32 s15, .L3
+	vmul.f32        s15, s14, s15
+	vmov    r0, s15
+	bx      lr
 .L3:
-  .word   1112014848
+	.float  50.0
 		
 
 Mul50Double:
-  vmov    d6, r0, r4
-  vldr.64 d7, .L6
-  vmul.f64        d7, d6, d7
-  vmov    r0, r1, d7
-  bx      lr
+	vmov    d6, r0, r4
+	vldr.64 d7, .L6
+	vmul.f64        d7, d6, d7
+	vmov    r0, r1, d7
+	bx      lr
 .L6:
-  .word   0
-  .word   1078525952
+	.double 50.0
 
 Data:
 	# Values
